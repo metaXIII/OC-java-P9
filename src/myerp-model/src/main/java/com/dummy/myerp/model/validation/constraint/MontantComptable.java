c9c83b34457bdc617813c3a3f1e6a51a9a4fed50
meta <gael.lehchibi@outlook.com>
2020-04-03 13:47:24 +0200

    correct dependencies for site

--- src/myerp-model/src/main/java/com/dummy/myerp/model/validation/constraint/MontantComptable.java
+++ src/myerp-model/src/main/java/com/dummy/myerp/model/validation/constraint/MontantComptable.java
@@ -1,52 +1,56 @@
 package com.dummy.myerp.model.validation.constraint;
 
-import java.lang.annotation.Documented;
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
 import javax.validation.Constraint;
 import javax.validation.Payload;
 import javax.validation.constraints.Digits;
+import java.lang.annotation.*;
 
 
 /**
  * Contrainte à apposer sur les attibuts de type "montant comptable"
- *
- *  Cette contrainte est composée de :
- *  <ul>
- *      <li>@{@link Digits}</li>
- *  </ul>
- *
- *  Types supportés :
- *  <ul>
- *      <li>{@link java.math.BigDecimal}</li>
- *  </ul>
+ * <p>
+ * ette contrainte est composée de :
+ * <ul>
+ *     <li>@{@link Digits}</li>
+ * </ul>
+ * <p>
+ * Types supportés :
+ * <ul>
+ *     <li>{@link java.math.BigDecimal}</li>
+ * </ul>
  */
 @Digits(integer = 13, fraction = 2)
-@Target({ ElementType.METHOD, ElementType.FIELD, ElementType.ANNOTATION_TYPE })
+@Target({ElementType.METHOD, ElementType.FIELD, ElementType.ANNOTATION_TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 @Constraint(validatedBy = {})
 @Documented
 public @interface MontantComptable {
 
-    /** Message de la violation */
+    /**
+     * @return Message de la violation
+     */
     String message() default "Taux de TVA invalide";
 
-    /** Groupe de validation */
+    /**
+     * @return Groupe de validation
+     */
     Class<?>[] groups() default {};
 
-    /** Payload */
+    /**
+     * @return Payload
+     */
     Class<? extends Payload>[] payload() default {};
 
     /**
      * Interface permettant la déclaration de plusieurs {@link MontantComptable}
      */
-    @Target({ ElementType.METHOD, ElementType.FIELD, ElementType.ANNOTATION_TYPE })
+    @Target({ElementType.METHOD, ElementType.FIELD, ElementType.ANNOTATION_TYPE})
     @Retention(RetentionPolicy.RUNTIME)
     @Documented
     @interface List {
-        /** List des {@link MontantComptable} */
+        /**
+         * @return List des {@link MontantComptable}
+         */
         MontantComptable[] value();
     }
 }
