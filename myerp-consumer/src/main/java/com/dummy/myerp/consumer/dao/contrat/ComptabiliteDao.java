759d9d9e82cf139845840853660195fb9d6b64c7
metaxiii <gael.lehchibi@outlook.com>
2020-05-08 19:27:40 +0200

    remove unused import

--- myerp-consumer/src/main/java/com/dummy/myerp/consumer/dao/contrat/ComptabiliteDao.java
+++ myerp-consumer/src/main/java/com/dummy/myerp/consumer/dao/contrat/ComptabiliteDao.java
@@ -3,7 +3,6 @@ package com.dummy.myerp.consumer.dao.contrat;
 import com.dummy.myerp.model.bean.comptabilite.CompteComptable;
 import com.dummy.myerp.model.bean.comptabilite.EcritureComptable;
 import com.dummy.myerp.model.bean.comptabilite.JournalComptable;
-import com.dummy.myerp.model.bean.comptabilite.SequenceEcritureComptable;
 import com.dummy.myerp.technical.exception.NotFoundException;
 
 import java.util.List;
